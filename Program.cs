3ceeeabec5c0d89a1cc524cd28142513803c7873
zelix <61917052+ada682@users.noreply.github.com>
2024-09-26 03:02:54 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -193,7 +193,7 @@ class ByBit
 
         for (int batch = 0; batch < totalBatches; batch++)
         {
-            int gameTime = new Random().Next(90, 120);
+            int gameTime = new Random().Next(90, 200);
             int score = new Random().Next(600, 900);
 
             var start = await StartGameAsync();
